57371819227456525f9cf8fd59a283f34f1eb269
Simon Slater <sslater11@gmail.com>
2025-05-18 17:45:13 +0100

    Fixed: Sprites delete properly when they go off the screen towards the left.

--- self_destroying_sprite.gd
+++ self_destroying_sprite.gd
@@ -7,24 +7,23 @@ var y_cut_off_bottom : float = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	# Delete sprite once it's off the left side of the screen
-	x_cut_off = get_viewport().get_camera_2d().global_position.x
-	x_cut_off -= (get_viewport_rect().size.x)
-	x_cut_off -= texture.get_width()
-
 	# Delete sprite that's above the screen.
 	y_cut_off_top = get_viewport().get_camera_2d().global_position.y
 	y_cut_off_top -= get_viewport_rect().size.y
-	y_cut_off_top -= texture.get_height()
-	
+	y_cut_off_top -= texture.get_height() * scale.y
 	
 	# Delete sprite that's below the screen.
 	y_cut_off_bottom = get_viewport().get_camera_2d().global_position.y
 	y_cut_off_bottom += get_viewport_rect().size.y
-	y_cut_off_bottom += texture.get_height()
+	y_cut_off_bottom += texture.get_height() * scale.y
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	# Delete sprite once it's off the left side of the screen
+	x_cut_off = get_viewport().get_camera_2d().global_position.x
+	x_cut_off -= (get_viewport_rect().size.x)
+	x_cut_off -= texture.get_width() * scale.x
+
 	if x_cut_off > global_position.x:
 		self.queue_free()
 	elif y_cut_off_top > global_position.y:

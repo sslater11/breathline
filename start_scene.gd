1c3eef33e7b980df00c9b124198c49371f48133c
Simon Slater <sslater11@gmail.com>
2026-01-31 21:35:14 +0000

    Added a time selector to the main screen.

--- start_scene.gd
+++ start_scene.gd
@@ -13,6 +13,12 @@ extends Node2D
 @onready var breathline: Path2D = $breathline
 @onready var wheel_1: Sprite2D = $breathline/PathFollow2D/car/wheel_1
 @onready var wheel_2: Sprite2D = $breathline/PathFollow2D/car/wheel_2
+@onready var time_up_button: Button = $timer/time_up_button
+@onready var time_down_button: Button = $timer/time_down_button
+@onready var label_selected_time_in_minutes: RichTextLabel = $timer/label_selected_time_in_minutes
+
+var breathline_in_minutes : int = 5
+
 
 func _ready() -> void:
 	line_road_marking.draw_breathline()
@@ -23,6 +29,12 @@ func _process(delta: float) -> void:
 	var wheel_speed : float = 5
 	wheel_1.rotate( delta * wheel_speed )
 	wheel_2.rotate( delta * wheel_speed )
+
+	if Input.is_action_just_pressed("ui_up"):
+		_on_time_up_button_pressed()
+		
+	if Input.is_action_just_pressed("ui_down"):
+		_on_time_down_button_pressed()
 
 func draw_ground() -> void:
 	var camera_rect : Rect2 = get_viewport_rect()
@@ -73,3 +85,31 @@ func draw_ground() -> void:
 		line.add_point( ending_point )
 		line.add_point( breathline_ending_point )
 		add_child( line )
+
+func update_breathline_length() -> void:
+	var total_seconds : int = 0
+	var total_minutes : int = 0
+	var total_breath_rounds : int = 0
+	while total_minutes < breathline_in_minutes:
+		total_seconds += Globals.get_length_of_a_full_breath_round_in_seconds()
+		total_minutes = floor( float(total_seconds) / 60.0 )
+		total_breath_rounds += 1
+		
+	total_breath_rounds += 1 # Add 1 to the total breaths to account for a full breath round at the start of the scene.
+	Globals.total_breath_rounds = int( total_breath_rounds )
+
+	label_selected_time_in_minutes.text = str( breathline_in_minutes )
+	if breathline_in_minutes == 1:
+		label_selected_time_in_minutes.text += " minute"
+	else:
+		label_selected_time_in_minutes.text += " minutes"
+
+func _on_time_up_button_pressed() -> void:
+	breathline_in_minutes += 1
+	update_breathline_length()
+
+func _on_time_down_button_pressed() -> void:
+	if breathline_in_minutes >= 2:
+		breathline_in_minutes -= 1
+		update_breathline_length()
+
